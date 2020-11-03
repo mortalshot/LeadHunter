4cd2dbd47548dd81636c5b3314d775f1f686d3f9
mortalshot <seregagl1996@gmail.com>
2020-11-03 16:32:38 +0300

    project init

---
+++ #src/js/index.js
@@ -0,0 +1,11 @@
+@@include('components/webp.js')
+@@include('components/menu.js')
+
+/*
+@@include('components/tabs.js')
+@@include('components/accordion.js')
+@@include('components/popup.js')
+@@include('components/header-fixed.js')
+
+@@include('components/media.js')
+*/
